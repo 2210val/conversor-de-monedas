8dfd504842e60d00eca68f4bf5e3be28e7c2c34a
2210val <167914197+2210val@users.noreply.github.com>
2024-05-03 14:18:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let inputs = document.querySelectorAll(".valor");
+
+let url = 'https://api.34ce5703629dcecc50591bb6/latest?symbols=USD,GBP,JPY';
+fetch(url)
+  .then(r => r.json())
+  .then(data => {
+    document.querySelector('#USD')
+      .dataset.cambio = data.rates.USD;
+    document.querySelector('#GBP')
+      .dataset.cambio = data.rates.GBP;  
+    document.querySelector('#JPY')
+      .dataset.cambio = data.rates.JPY;
+
+    inputs.forEach(input => {
+      input.value = input.dataset.cambio;
+    });
+  })
+  .catch(error => console.error(error))
+
+function valorCambiado(input) {
+  let factor = input.value / input.dataset.cambio;
+  inputs.forEach(campo => {
+    campo.value = (campo.dataset.cambio * factor).toFixed(2);
+  })
+}
